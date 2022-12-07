cafa124eda63f8da5a8a9546cf6e8c1ffe7df975
Guilherme-Fernandes97 <guilherme_fernandes1997@hotmail.com>
2022-12-06 22:11:51 -0300

    adicionando paginas de detalhes filme e home filmes

--- filme/views.py
+++ filme/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import Filme
+from django.views.generic import TemplateView, CreateView, ListView, DetailView
 
 # Create your views here.
 
-def homepage(request):
-    return render(request, "homepage.html")
+class Homepage(TemplateView):
+    template_name = "homepage.html"
+
+
+class Homefilmes(ListView):
+    template_name = "homefilmes.html"
+    model = Filme
+    # object_list = lista de itens do modelo
+
+class Detalhesfilme(DetailView):
+    template_name = "detalhesfilme.html"
+    model = Filme
+    # object = 1 item do nosso modelo
